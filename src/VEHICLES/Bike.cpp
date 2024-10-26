811186be9a050aed9dc2ff067b3abb4a5e0341d1
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-10-26 18:38:48 +0530

    Add files via upload

---
+++ src/VEHICLES/Bike.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Bike.h"
+
+std::string Bike::get_vehicle_type() const 
+{
+    return "Bike"; // Directly returning the type
+}
---
+++ src/VEHICLES/Car.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Car.h"
+
+std::string Car::get_vehicle_type() const {
+    return "Car"; // Directly returning the type
+}
